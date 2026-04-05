8291e5b8b604dde8ab1d7bb5b5edd9009a1e58d3
Nishchalmishra <nishchalmishra14@gmail.com>
2026-04-05 16:41:38 +0530

    update / route

--- server/src/lib/utils/multer.js
+++ server/src/lib/utils/multer.js
@@ -1,3 +1,17 @@
-import multer from "multer";
+// import multer from "multer";
 
-export const upload = multer({ dest: "/uploads" });
+// export const upload = multer({ dest: "/uploads" });
+
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/"); // ✅ relative path
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+export const upload = multer({ storage });
